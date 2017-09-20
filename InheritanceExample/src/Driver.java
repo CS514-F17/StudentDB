3efd3e6016a5a29e26649d875f176ae631b08175
Sami Rollins <srollins@cs.usfca.edu>
2017-09-20 11:40:35 -0700

    update class examples

---
+++ InheritanceExample/src/Driver.java
@@ -0,0 +1,15 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		//Beverage b = new Beverage(10); //impossible!
+		Beverage b = new Coffee(8, false);
+		b.getServingGlass();
+		
+//		System.out.println(c.toString());
+//		System.out.println(c.getSize());
+//		System.out.println(c.getServingGlass());
+	}
+
+}
